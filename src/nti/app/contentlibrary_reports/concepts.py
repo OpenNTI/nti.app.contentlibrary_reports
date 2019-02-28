16fc47d48a94a811a2a5f54a6ea1c26cda469498
Egawati Panjei <egawati.panjei@gmail.com>
2019-02-28 14:45:00 +0700

    count estimate reading time for a concept tree

---
+++ src/nti/app/contentlibrary_reports/concepts.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import math
+
+from zope import interface
+
+from nti.app.contentlibrary_reports.interfaces import IConcepts
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(IConcepts)
+class Concepts(object):
+    def process(self, context):
+        fs_root = context.ContentPackages[0]
+        sibling = fs_root.make_sibling_key('concepts.json')
+        concepts = sibling.readContentsAsJson()
+        return concepts
+
+
+class ConceptsHierarchyTraversal(object):
+    def __init__(self, concepts):
+        self.concepts = concepts
+
+
+class ConceptsEstimatedReadingTime(object):
+    def __init__(self, concept_hierachy, cmtime):
+        """
+        concept_hierarcy is a dictionary loaded from concepts.json
+        cmtime is an object of ContentConsumptionTime
+        """
+        self.concept_hierachy = concept_hierachy
+        self.cmtime = cmtime
+
+    def process(self):
+        concepts_metrics = {}
+        if 'concepthierarchy' in self.concept_hierachy:
+            tree = self.concept_hierachy['concepthierarchy']
+            if 'concepts' in tree:
+                concepts = tree['concepts']
+                for item in concepts:
+                    self._traverse_concept_tree(item, concepts[item], concepts_metrics)
+        return concepts_metrics
+
+    def _traverse_concept_tree(self, concept_ntiid, concept, concepts_metrics):
+        cmetrics = {}
+        cmetrics['name'] = concept['name']
+        self._count_content_unit_metrics(cmetrics, concept['contentunitntiids'])
+        concepts_metrics[concept_ntiid] = cmetrics
+        if 'concepts' in concept:
+            subconcepts = concept['concepts']
+            for item in subconcepts:
+                self._traverse_concept_tree(item, subconcepts[item], concepts_metrics)
+                self._rollup_estimated_reading_time(concept_ntiid, item, concepts_metrics)
+
+    def _count_content_unit_metrics(self, cmetric, content_unit_ntiids):
+        cmetric['normalized_estimated_reading_time'] = 0
+        cmetric['estimated_reading_time'] = 0
+        for unit_ntiid in content_unit_ntiids:
+            cmetric['normalized_estimated_reading_time'] += self.cmtime.get_normalize_estimated_time_in_minutes(unit_ntiid)
+            cmetric['estimated_reading_time'] += self.cmtime.get_total_minutes(unit_ntiid)
+
+    def _rollup_estimated_reading_time(self, cparent_ntiid, cchild_ntiid, concepts_metrics):
+        concepts_metrics[cparent_ntiid]['normalized_estimated_reading_time'] += concepts_metrics[cchild_ntiid]['normalized_estimated_reading_time']
+        concepts_metrics[cparent_ntiid]['estimated_reading_time'] += concepts_metrics[cchild_ntiid]['estimated_reading_time']
---
+++ src/nti/app/contentlibrary_reports/tests/test_concepts.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from hamcrest import assert_that
+from hamcrest import is_
+from hamcrest import has_entries
+
+import os
+import json
+import math
+import unittest
+
+from nti.app.contentlibrary_reports.concepts import Concepts
+from nti.app.contentlibrary_reports.concepts import ConceptsEstimatedReadingTime
+from nti.app.contentlibrary_reports.content_metrics import ContentConsumptionTime
+
+from nti.app.contentlibrary_reports.interfaces import IConcepts
+from nti.testing.matchers import verifiably_provides
+
+
+class TestConcepts(unittest.TestCase):
+
+    def test_concepts(self):
+        concepts = Concepts()
+        assert_that(concepts, verifiably_provides(IConcepts))
+
+
+class TestConceptsEstimatedReadingTime(unittest.TestCase):
+    def data_file(self, name):
+        return os.path.join(os.path.dirname(__file__), 'data', name)
+
+    def test_concepts_estimated_reading_time(self):
+        concepts_file = 'concepts.json'
+        content_metrics_file = 'content_metrics.json'
+
+        with open(self.data_file(content_metrics_file)) as fp:
+            content_metrics = json.load(fp)
+        tparams = {'minute_block': 15,
+                   'wpm': 200,
+                   'figure_n_word': 0,
+                   'table_n_word': 0,
+                   'image_n_word': 0}
+        cmtime = ContentConsumptionTime(content_metrics, tparams)
+        with open(self.data_file(concepts_file)) as fp:
+            concepts_hierarchy = json.load(fp)
+
+        certime = ConceptsEstimatedReadingTime(concepts_hierarchy, cmtime)
+        concepts_metrics = certime.process()
+        concept_1_ntiid = "tag:nextthought.com,2011-10:IFSTA-NTIConcept-sample_book.concept.concept:NFPA_1072"
+        concept_2_ntiid = "tag:nextthought.com,2011-10:IFSTA-NTIConcept-sample_book.concept.concept:math"
+
+        chapter_1_ntiid = "tag:nextthought.com,2011-10:IFSTA-HTML-sample_book.chapter:1"
+        chapter_2_ntiid = "tag:nextthought.com,2011-10:IFSTA-HTML-sample_book.chapter:2"
+
+        chapter_1_reading_time_in_minutes = cmtime.get_total_minutes(chapter_1_ntiid)
+        chapter_2_reading_time_in_minutes = cmtime.get_total_minutes(chapter_2_ntiid)
+
+        assert_that(concepts_metrics, has_entries(concept_1_ntiid,
+                                                  has_entries('name', is_(u'NFPA 1072'),
+                                                              'estimated_reading_time', is_(0)
+                                                              )
+                                                  )
+                    )
+        assert_that(concepts_metrics, has_entries(concept_2_ntiid,
+                                                  has_entries('name', is_(u'math'),
+                                                              'estimated_reading_time', is_(chapter_1_reading_time_in_minutes + chapter_2_reading_time_in_minutes))
+                                                  )
+                    )
+
+        chapter_1_normalized_reading_time_in_minutes = cmtime.get_normalize_estimated_time_in_minutes(chapter_1_ntiid)
+        chapter_2_normalized_reading_time_in_minutes = cmtime.get_normalize_estimated_time_in_minutes(chapter_2_ntiid)
+        total_normalized_reading_time_in_minutes = chapter_1_normalized_reading_time_in_minutes + chapter_2_normalized_reading_time_in_minutes
+
+        assert_that(concepts_metrics, has_entries(concept_2_ntiid,
+                                                  has_entries('name', is_(u'math'),
+                                                              'normalized_estimated_reading_time', is_(total_normalized_reading_time_in_minutes))
+                                                  )
+                    )
