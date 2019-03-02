39d8f1fc12885204d58d4ce1f6a74be66d5437e4
Egawati Panjei <egawati.panjei@gmail.com>
2019-03-02 12:24:47 +0700

    use namedtuple, add comment

--- src/nti/app/contentlibrary_reports/concepts.py
+++ src/nti/app/contentlibrary_reports/concepts.py
@@ -10,6 +10,8 @@ from __future__ import absolute_import
 
 import math
 
+from collections import namedtuple
+
 from zope import interface
 
 from nti.app.contentlibrary_reports.interfaces import IConcepts
@@ -24,6 +26,9 @@ class Concepts(object):
         sibling = fs_root.make_sibling_key('concepts.json')
         concepts = sibling.readContentsAsJson()
         return concepts
+
+
+_ConceptMetric = namedtuple('ConceptMetric', ('estimated_reading_time', 'normalized_estimated_reading_time'))
 
 
 class ConceptsEstimatedReadingTime(object):
@@ -46,23 +51,36 @@ class ConceptsEstimatedReadingTime(object):
         return concepts_metrics
 
     def _traverse_concept_tree(self, concept_ntiid, concept, concepts_metrics):
-        cmetrics = {}
-        cmetrics['name'] = concept['name']
-        self._count_estimated_reading_time(cmetrics, concept['contentunitntiids'])
-        concepts_metrics[concept_ntiid] = cmetrics
+        cmetric = {}
+        cmetric['name'] = concept['name']
+        metrics = self._count_estimated_reading_time(concept['contentunitntiids'])
+        cmetric['normalized_estimated_reading_time'] = metrics.normalized_estimated_reading_time
+        cmetric['estimated_reading_time'] = metrics.estimated_reading_time
+        concepts_metrics[concept_ntiid] = cmetric
         if 'concepts' in concept:
             subconcepts = concept['concepts']
             for subconcept_ntiid, subconcept in subconcepts.items():
                 self._traverse_concept_tree(subconcept_ntiid, subconcept, concepts_metrics)
                 self._rollup_estimated_reading_time(concept_ntiid, subconcept_ntiid, concepts_metrics)
 
-    def _count_estimated_reading_time(self, cmetric, content_unit_ntiids):
-        cmetric['normalized_estimated_reading_time'] = 0
-        cmetric['estimated_reading_time'] = 0
+    def _count_estimated_reading_time(self, content_unit_ntiids):
+        """
+        Method to count estimated(normalized) reading time given a list of content unit ntiids (content_unit_ntiids) where a concept is referred.
+        Estimated reading time is computed based on the total number of words in a content unit. It is done by ContentConsumptionTime object.
+        Normalized estimated reading time is estimated reading time round up to the nearest minute block (the default is 15).
+        This method returns a namedtuple _ConceptMetric
+        """
+        normalized_est_rtime = 0
+        est_rtime = 0
         for unit_ntiid in content_unit_ntiids:
-            cmetric['normalized_estimated_reading_time'] += self.cmtime.get_normalize_estimated_time_in_minutes(unit_ntiid)
-            cmetric['estimated_reading_time'] += self.cmtime.get_total_minutes(unit_ntiid)
+            est_rtime += self.cmtime.get_total_minutes(unit_ntiid)
+            normalized_est_rtime += self.cmtime.get_normalize_estimated_time_in_minutes(unit_ntiid)
+        metrics = _ConceptMetric(est_rtime, normalized_est_rtime)
+        return metrics
 
     def _rollup_estimated_reading_time(self, cparent_ntiid, cchild_ntiid, concepts_metrics):
+        """
+        This method add up estimated(normalized) reading time from a child/sub concept to its parent concept
+        """
         concepts_metrics[cparent_ntiid]['normalized_estimated_reading_time'] += concepts_metrics[cchild_ntiid]['normalized_estimated_reading_time']
         concepts_metrics[cparent_ntiid]['estimated_reading_time'] += concepts_metrics[cchild_ntiid]['estimated_reading_time']
